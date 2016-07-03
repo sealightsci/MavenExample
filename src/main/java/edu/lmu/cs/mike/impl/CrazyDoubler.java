c42015dc5289a372ce8d2b451e4995ca506dec1b
Niv Tschaikov <niv@sealights.io>
2016-07-03 18:57:23 +0300

    fix

--- src/main/java/edu/lmu/cs/mike/impl/CrazyDoubler.java
+++ src/main/java/edu/lmu/cs/mike/impl/CrazyDoubler.java
@@ -2,7 +2,7 @@ package edu.lmu.cs.mike.impl;
 
 import edu.lmu.cs.mike.iface.IntegerDoubler;
 
-public class crazyDoubler implements IntegerDoubler {
+public class CrazyDoubler implements IntegerDoubler {
 
   @Override
   public long doDouble(int i) {
